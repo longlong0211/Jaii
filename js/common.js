de74f9a9a5f0b5e67dd3ea760c84ab3cd54009e9
xulong <577531920@qq.com>
2016-07-04 15:10:40 +0800

    初始化终端信息

--- js/common.js
+++ js/common.js
@@ -1,11 +1,30 @@
-function postData(url, data, callback) {
-	url = "http://192.168.199.220:8080"+url;
-    mui.ajax(url,{  
+var base_server = "http://192.168.199.220:8080";
+var k,version;
+var version_key = "version";
+var k_key = "k";
+
+function initVersion(){
+	 plus.runtime.getProperty(plus.runtime.appid,function(inf){
+	 	plus.storage.setItem(version_key,inf.version);
+	});
+};
+
+function postData(url, plus, data, callback) {
+	k = plus.storage.getItem(k_key);
+	if(k==null){
+		k="";
+	}
+	version = plus.storage.getItem(version_key);
+	if(version==null){
+		version="1.0.0";
+	}
+	var headers = {"udid":plus.device.uuid,"name":plus.os.name,
+					"version":version,"k":k};
+    mui.ajax(base_server+url,{  
         data:data,  
         dataType:'json',  
         type:'post',  
-        headers:{"udid":"5362A6FB-6E18-425D-8E11-A4E088F85E71",
-        			"name":"iphone"},
+        headers:headers,
         contentType:"application/x-www-form-urlencoded; charset=utf-8",  
         timeout:60000,  
         async:true,
